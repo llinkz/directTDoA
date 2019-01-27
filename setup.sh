c0ce954dd0fd30078e6813b2974cfe30ae401dad
llinkz <ounaid@gmail.com>
2019-01-27 19:00:59 +0100

    new setup & file patchs

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 python -m pip install numpy pillow requests pygame scipy
 cd TDoA/kiwiclient
 mkoctfile read_kiwi_iq_wav.cc
-cd ../m
-patch -i ../../nopng.diff tdoa_plot_map.m
 cd ../..
+patch -i kiwiworker_patch.diff ./TDoA/kiwiclient/kiwiworker.py
+patch -i plot_map_patch.diff ./TDoA/m/tdoa_plot_map.m
+echo -e "The setup is now finished.\nTo start the software from console, type ./directTDoA.py"
+sleep 5
